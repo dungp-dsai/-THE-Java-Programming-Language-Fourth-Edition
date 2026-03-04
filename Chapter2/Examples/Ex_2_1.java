07f2761fa0679221bb74c6e13e4dfe71ba784391
AnhDungPham290198 <dungpham290198@gmail.com>
2026-03-04 11:05:44 +0700

    add chapter 2

---
+++ Chapter2/Examples/Ex_2_1.java
@@ -0,0 +1,8 @@
+package Chapter2.Examples;
+
+public class Ex_2_1 {
+    final static int a;
+    static {
+        a = 1;
+    }
+}
---
+++ Chapter2/Exercises/Vehicle2.java
@@ -0,0 +1,4 @@
+public class Vehicle2 {
+    public static int next_id_number;
+    public int car_id;
+}
